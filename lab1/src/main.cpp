d76ed965b1e346f41a059b859fc5e0f4d77c444f
wrnch <no@mail.com>
2023-02-18 16:10:23 +0200

    Lab1 Add files and folders counter

--- lab1/src/main.cpp
+++ lab1/src/main.cpp
@@ -5,6 +5,37 @@
 
 using std::cout, std::endl;
 
+void count_and_print_files(FilesystemElement directory, int *prev_dirs = nullptr, int *prev_files = nullptr)
+{
+    bool is_first = false;
+    if (prev_dirs == nullptr && prev_files == nullptr)
+    {
+        is_first = true;
+        prev_dirs = new int;
+        *prev_dirs = 0;
+        prev_files = new int;
+        *prev_files = 0;
+    }
+    for (auto &el : directory.elements)
+    {
+        if (el.is_directory())
+        {
+            (*prev_dirs)++;
+            count_and_print_files(el, prev_dirs, prev_files);
+        }
+        else
+        {
+            (*prev_files)++;
+        }
+    }
+    if (is_first)
+    {
+        cout << "Files: " << *prev_files << ", Folders: " << *prev_dirs << endl;
+        delete prev_dirs;
+        delete prev_files;
+    }
+}
+
 void print_directory_recursive(FilesystemElement listing, int layer = 0)
 {
     for (auto &el : listing.elements)
@@ -41,6 +72,7 @@ int main()
     auto listing = list_directory(current_path);
 
     print_directory_recursive(listing);
+    count_and_print_files(listing);
 
     return 0;
 }
