c0c4ddf947a02e4e634f8eafb07f37734f0fc144
wrnch <no@mail.com>
2023-02-18 13:33:42 +0200

    Initial commit. Lab 1 cmake template

---
+++ lab1/src/lib.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+
+#include "lib.hpp"
+
+void hello()
+{
+    std::cout << "Hello World" << std::endl;
+}
---
+++ lab1/src/main.cpp
@@ -0,0 +1,8 @@
+#include "lib.hpp"
+
+int main()
+{
+    hello();
+
+    return 0;
+}
